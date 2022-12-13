c1506aa22120ce93aeda0186b4b9a6b30ec1d593
denyAndriana0107 <denyandriana06@gmail.com>
2022-12-14 03:07:59 +0700

    schedule create presensi 3.09 AM

--- app/routes/presensi/siswa/JobScheduleCreatePresensi.js
+++ app/routes/presensi/siswa/JobScheduleCreatePresensi.js
@@ -3,22 +3,34 @@ module.exports = app => {
     var getTimeStamp = require("../../../helper/GetTimeStamps");
     var CekLibur = require("../../../helper/CheckHoliday");
 
-    const schedule = require('node-schedule');
-    const rule = new schedule.RecurrenceRule();
-    rule.hour = 2;
-    rule.minute = 41;
-    rule.tz = 'Asia/Jakarta';
-    console.log("run");
-    schedule.scheduleJob(rule, async (error) => {
-        console.log("run");
+    // const schedule = require('node-schedule');
+    // const rule = new schedule.RecurrenceRule();
+    // rule.hour = 2;
+    // rule.minute = 41;
+    // rule.tz = 'Asia/Jakarta';
+    // console.log("run");
+    // schedule.scheduleJob(rule, async (error) => {
+    //     if (CekLibur.result(getTimeStamp.timestamp()) == false && getTimeStamp.day() != 0) {
+    //         PresensiModel.create((err, result) => {
+    //             if (err) {
+    //                 throw err;
+    //             }
+    //         });
+    //     }
+    // });
+    var cron = require('node-cron');
+
+    cron.schedule('9 3 * * *', () => {
         if (CekLibur.result(getTimeStamp.timestamp()) == false && getTimeStamp.day() != 0) {
             PresensiModel.create((err, result) => {
                 if (err) {
-                    throw err;
+                    console.log(err);
                 }
             });
         }
+    }, {
+        scheduled: true,
+        timezone: "Asia/Jakarta"
     });
-
 
 }
