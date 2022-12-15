5f4058df0c3899afb6685deacc78a9e857dfc12e
denyAndriana0107 <denyandriana06@gmail.com>
2022-12-15 14:40:38 +0700

    fixes semester middleware

--- app/middleware/nilai/NilaiMiddleware.js
+++ app/middleware/nilai/NilaiMiddleware.js
@@ -8,11 +8,12 @@ module.exports = {
         next();
     },
     semester: (req, res, next) => {
-        if (!req.body.semester == 0 || !req.body.semester == 1) {
+        if (req.body.semester != 0 && req.body.semester != 1) {
             return res.status(500).send({
                 message: `value semester \n semester = 0 untuk genap dan semester = 1 untuk ganjil`
             })
         }
+        next();
     }
 
 }
